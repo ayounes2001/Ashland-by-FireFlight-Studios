fe090daa9846b97a77702ddac0e33afc3821f4b5
ayounes2001 <1016744@student.sae.edu.au>
2020-10-08 19:27:15 +1100

    Movement with New Input System and some Camera Stuff

--- Project Test/Assets/Team Members worskpace/Anthony/CameraFollow.cs
+++ Project Test/Assets/Team Members worskpace/Anthony/CameraFollow.cs
@@ -1,26 +1,73 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player;
-    private Vector3 velocity = Vector3.zero;
-    
-    [Header("Camera Variables")]
-    public float smooth = 0.3f;
-    public float height;
-    public float zAxis = 10f;
+    [Header("Rotate Camera")]
+    private CameraMovement controls;
+    public float turnSpeed = 4.0f;
+    public Transform followTransform;
 
+    private Vector2 rotate;
+
+
+    private void Awake()
+    {
+        controls = new CameraMovement();
+
+        controls.Camera.CameraRotate.performed += ctx => rotate = ctx.ReadValue<Vector2>();
+ 
+        controls.Camera.CameraRotate.canceled += ctx => rotate = Vector2.zero;
+    }
+
+    void Start()
+    {
+       
+
+    }
+
+    
 
     private void Update()
     {
-        Vector3 pos = new Vector3();
-        pos.x = player.position.x;
-        pos.z = player.position.z - zAxis;
-        pos.y = player.position.y + height;
-        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smooth);
+       
+        followTransform.transform.rotation *= Quaternion.AngleAxis(rotate.x*turnSpeed,Vector3.up);
+        followTransform.transform.rotation *= Quaternion.AngleAxis(rotate.y * turnSpeed, Vector3.right);
 
+        var angles = followTransform.transform.localEulerAngles;
+        angles.z = 0;
+        var angle = followTransform.transform.localEulerAngles.x;
+
+        if (angle > 180 && angle < 340)
+        {
+            angles.x = 340;
+        }
+        else if (angle < 180 && angle > 40)
+        {
+            angles.x = 40;
+        }
+
+        followTransform.transform.localEulerAngles = angles;
+
+
+        transform.rotation = Quaternion.Euler(0, followTransform.transform.rotation.eulerAngles.y, 0);
+
+        followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+    }
+    
+    void OnEnable()
+    {
+        controls.Camera.Enable();
+    }
+ 
+    void OnDisable()
+    {
+        controls.Camera.Disable();
     }
 }
+
+
---
+++ Project Test/Assets/Team Members worskpace/Anthony/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerControls controls;
+    public float movementSpeed;
+    private Vector2 movementinput;
+    private Vector3 moveVector;
+    private Vector3 inputDirection;
+
+    private Quaternion currentRotation;
+    
+    [Header("Jump Variables")]
+    public float jumpForce;
+    public float raycastDistance;
+    private void OnEnable()
+    {
+        controls.GamePlayer.Enable();
+    }
+
+    private void OnDisable()
+    {
+     controls.GamePlayer.Disable();   
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controls = new PlayerControls();
+    }
+
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+    //QUICK SHIT HACK
+    var movementInput = controls.GamePlayer.Movement.ReadValue<Vector2>();
+    var movement = new Vector3
+    {
+        x = movementInput.x,
+        z = movementInput.y
+    }.normalized;
+    
+    transform.Translate(movement * (movementSpeed * Time.deltaTime),Space.World);
+    transform.rotation = Quaternion.LookRotation(movement);
+    }
+    
+    public void Jump(InputAction.CallbackContext obj)
+    {
+        if (IsGrounded())
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+    }
+
+    private bool IsGrounded()
+    {
+        Debug.DrawRay(transform.position,Vector3.down * raycastDistance,Color.blue);
+        return Physics.Raycast(transform.position,Vector3.down,raycastDistance);
+    }
+}
