acfe851ea9df55d762b2d028e795cffcedf4fcb3
ayounes2001 <1016744@student.sae.edu.au>
2020-11-20 11:56:07 +1100

    Basic Camera work press E to look back

--- Ashland/Assets/Team Members worskpace/Naomi/Scripts/DemoCamManager.cs
+++ Ashland/Assets/Team Members worskpace/Naomi/Scripts/DemoCamManager.cs
@@ -10,7 +10,20 @@ public class DemoCamManager : MonoBehaviour
 
     void LateUpdate()
     {
-        if (Input.GetAxis("Vertical") < 0) { MainCam.SetActive(false); FireCam.SetActive(true); }
-        else if (Input.GetAxis("Vertical") > 0) { FireCam.SetActive(false); MainCam.SetActive(true); }
+        // if (Input.GetAxis("Vertical") < 0) { MainCam.SetActive(false); FireCam.SetActive(true); }
+        // else if (Input.GetAxis("Vertical") > 0) { FireCam.SetActive(false); MainCam.SetActive(true); }
+
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            MainCam.SetActive(false);
+            FireCam.SetActive(true);
+        }
+        else if(Input.GetKeyUp(KeyCode.E))
+        {
+            MainCam.SetActive(true);
+            FireCam.SetActive(false);
+        }
+
     }  
 }
