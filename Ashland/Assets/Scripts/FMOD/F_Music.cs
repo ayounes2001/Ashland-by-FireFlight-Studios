5f8fe05a368075d536e0b125a65bafd6584072cf
Kyan Rowse <rowse9@gmail.com>
2020-12-14 17:37:23 +1100

    Music and Footsteps

---
+++ Ashland/Assets/Scripts/FMOD/F_Music.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class F_Music : MonoBehaviour
+{
+    private EventInstance music;
+    void Start()
+    {
+        music = RuntimeManager.CreateInstance("event:/Music/Music");
+        music.start();
+        music.release();
+    }
+}
