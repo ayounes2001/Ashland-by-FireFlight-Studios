4860fd37da923c9e228f38b9d928b88843bfa740
naomiwholt <54776733+naomiwholt@users.noreply.github.com>
2020-12-20 21:32:35 +1100

    You can turn on a burning tree with fir music with "T" and "R"
    I don't know how to stop the background music from playing though :'(

--- Ashland/Assets/Team Members worskpace/Naomi/Scripts/CharactersOnOff.cs
+++ Ashland/Assets/Team Members worskpace/Naomi/Scripts/CharactersOnOff.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class CharactersOnOff : MonoBehaviour
 {
     public GameObject CharacterModels;
+    public GameObject SamundrasModels;
+    public bool SamundrasModelsOn = false;
     public bool CharactersOn = true;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z) && CharactersOn == false) { CharactersOn = true; print("turn on Characters"); CharacterModels.SetActive(true); }
+        if (Input.GetKeyDown(KeyCode.Z) && CharactersOn == false) { CharactersOn = true; SamundrasModelsOn = false; print("turn on Characters"); CharacterModels.SetActive(true); SamundrasModels.SetActive(false); }
 
-        if (Input.GetKeyDown(KeyCode.X) && CharactersOn == true) { CharactersOn = false; print("turn off Characters"); CharacterModels.SetActive(false); }
+        if (Input.GetKeyDown(KeyCode.X) && CharactersOn == true) { CharactersOn = false; SamundrasModelsOn = true; print("turn off Characters"); CharacterModels.SetActive(false); SamundrasModels.SetActive(true); }
     }
 }
