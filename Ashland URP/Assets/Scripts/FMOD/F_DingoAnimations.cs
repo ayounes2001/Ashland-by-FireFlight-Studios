33c0c7324c0d19666de762ecdfbae7a5a7cbb615
ayounes2001 <1016744@student.sae.edu.au>
2021-02-11 14:45:54 +1100

    ASHLAND URP VERSION

---
+++ Ashland URP/Assets/Scripts/FMOD/F_DingoAnimations.cs
@@ -0,0 +1,14 @@
+﻿using FMODUnity;
+using UnityEngine;
+
+public class F_DingoAnimations : MonoBehaviour
+{
+    public void DingoStep()
+    {
+        RuntimeManager.PlayOneShot("event:/Dingo/Footsteps");
+    }
+    public void DingoStepRun()
+    {
+        
+    }
+}
